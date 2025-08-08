20ac449606b35bb13d4cf9461f0ed4abb519fb34
gimmick72 <finesj101@gmail.com>
2025-08-08 23:54:26 +0700

    Update index.tsx

--- login/src/pages/Booking/index.tsx
+++ login/src/pages/Booking/index.tsx
@@ -29,29 +29,20 @@ const Booking = () => {
           คุณต้องการทำอะไร
         </div>
 
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            flexWrap: "wrap",
-            gap: "2rem",
-            top: "50%",
-          }}
-        >
-          <Link to="/booking/select-queue">
-            <Card
-              hoverable
-              style={{ width: 300, height: 400, marginRight: "2rem" }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://tse3.mm.bing.net/th/id/OIP.MHcgIR-4n7KYqKvFE3hp0QHaE7?cb=thfvnext&rs=1&pid=ImgDetMain&o=7&rm=3"
-                />
-              }
-            >
-              <Meta title="จัดฟัน" description="dhjsgdhgsj" />
-            </Card>
-          </Link>
+        <Link to="/booking/queue">
+      <Card
+        hoverable
+        style={{ width: 300, height: 400, marginRight: "2rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://tse3.mm.bing.net/th/id/OIP.MHcgIR-4n7KYqKvFE3hp0QHaE7?cb=thfvnext&rs=1&pid=ImgDetMain&o=7&rm=3"
+          />
+        }
+      >
+        <Meta title="จัดฟัน" description="dhjsgdhgsj" />
+      </Card>
+    </Link>
           <Link to="/booking/select-queue">
             <Card
               hoverable
@@ -82,7 +73,6 @@ const Booking = () => {
             </Card>
           </Link>
         </div>
-      </div>
     </>
   );
 };
