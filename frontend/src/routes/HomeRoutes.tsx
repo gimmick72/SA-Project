e88958b37498500abb05fb1b2620a7957f27f66e
Khanathip <khanathip_kpb@hotmail.com>
2025-08-24 20:45:38 +0700

    Refactor: Restructure project files and remove old pages

--- frontend/src/routes/HomeRoutes.tsx
+++ frontend/src/routes/HomeRoutes.tsx
@@ -1,17 +1,17 @@
-import { lazy } from "react";
+import { lazy } from "react"; 
 import type { RouteObject } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import IndexLayout from "../layout/IndexLayout";
 import Loadable from "../components/third-patry/Loadable";
 
-const HomePage = Loadable(lazy(() => import("../pages/index_page/index")));
-const BookingPage = Loadable(lazy(() => import("../pages/index_page/booking_page/index")));
-const BookingQueue = Loadable(lazy(() => import("../pages/index_page/booking_page/Booking")));
+const HomePage = Loadable(lazy(() => import("../pages/HomePage/index_page/index")));
+const BookingPage = Loadable(lazy(() => import("../pages/HomePage/index_page/booking_page/index")));
+const BookingQueue = Loadable(lazy(() => import("../pages/HomePage/index_page/booking_page/Booking")));
 
-const OurDentist = Loadable(lazy(() => import("../pages/index_page/OurDentists/index")));
-const ContactUs = Loadable(lazy(() => import("../pages/index_page/ContactUs/index")));
-const PriceGuide = Loadable(lazy(() => import("../pages/index_page/PriceGuide/index")));
-const ServicePage = Loadable(lazy(() => import("../pages/index_page/Services/index")));
+const OurDentist = Loadable(lazy(() => import("../pages/HomePage/index_page/OurDentists/index")));
+const ContactUs = Loadable(lazy(() => import("../pages/HomePage/index_page/ContactUs/index")));
+const PriceGuide = Loadable(lazy(() => import("../pages/HomePage/index_page/PriceGuide/index")));
+const ServicePage = Loadable(lazy(() => import("../pages/HomePage/index_page/Services/index")));
 
 const IndexRoutes: RouteObject[] = [
   {
--- frontend/src/routes/StaffRoutes.tsx
+++ frontend/src/routes/StaffRoutes.tsx
@@ -3,22 +3,22 @@ import type { RouteObject } from "react-router-dom";
 import Loadable from "../components/third-patry/Loadable";
 import FullLayout from "../layout/FullLayout";
 
-const IndexPage = Loadable(lazy(() => import("../pages/index_page/index")));
-const HomeInfoPage = Loadable(lazy(() => import("../pages/home_info/index")));
-const PatientInfoPage = Loadable(lazy(() => import("../pages/patient_info/index"))); 
-const TreatmentInfoPage = Loadable(lazy(() => import("../pages/treatment_info/index")));
-const ScheduleInfoPage = Loadable(lazy(() => import("../pages/schedule_info/index")));
-const MedicineInfoPage = Loadable(lazy(() => import("../pages/medicine_page/index")));
-const StaffInfoPage = Loadable(lazy(() => import("../pages/staff_info/index")));
-const QueueInfoPage = Loadable(lazy(() => import("../pages/queue_info/index")));
-const PaymentInfoPage = Loadable(lazy(() => import("../pages/payment_info/index")));
-const AttendanceInfoPage = Loadable(lazy(() => import("../pages/attendance_info/index")));
-const ServiceInfoPage = Loadable(lazy(() => import("../pages/service_info/index")));
+const IndexPage = Loadable(lazy(() => import("../pages/HomePage/index_page/index")));
+const HomeInfoPage = Loadable(lazy(() => import("../pages/HomePage/home_info/index")));
+const PatientInfoPage = Loadable(lazy(() => import("../pages/HomePage/patient_info/index"))); 
+const TreatmentInfoPage = Loadable(lazy(() => import("../pages/HomePage/treatment_info/index")));
+const ScheduleInfoPage = Loadable(lazy(() => import("../pages/HomePage/schedule_info/index")));
+const MedicineInfoPage = Loadable(lazy(() => import("../pages/HomePage/medicine_page/index")));
+const StaffInfoPage = Loadable(lazy(() => import("../pages/HomePage/staff_info/index")));
+const QueueInfoPage = Loadable(lazy(() => import("../pages/HomePage/queue_info/index")));
+const PaymentInfoPage = Loadable(lazy(() => import("../pages/HomePage/payment_info/index")));
+const AttendanceInfoPage = Loadable(lazy(() => import("../pages/HomePage/attendance_info/index")));
+const ServiceInfoPage = Loadable(lazy(() => import("../pages/HomePage/service_info/index")));
 
 //patient routes
-const AddPatient = Loadable(lazy(() => import("../pages/patient_info/AddPatientPage")));
-const InitialSymptoms = Loadable(lazy(() => import("../pages/patient_info/InitialPage")));
-const PatienTable = Loadable(lazy(() => import("../pages/patient_info/component_patient/table")));
+const AddPatient = Loadable(lazy(() => import("../pages/HomePage/patient_info/AddPatientPage")));
+const InitialSymptoms = Loadable(lazy(() => import("../pages/HomePage/patient_info/InitialPage")));
+const PatienTable = Loadable(lazy(() => import("../pages/HomePage/patient_info/component_patient/table")));
 
 
 const AdminRoutes: RouteObject[] = [
