dc1a696f9ce365f43c200867974707b72caf02b3
Khanathip <khanathip_kpb@hotmail.com>
2025-09-01 11:37:36 +0700

    Update: pull branch: New-Gim to branch: Khanathip

--- frontend/src/layout/IndexLayout.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Outlet } from "react-router-dom";
-import Layout from "antd/es/layout";
-import Navbar from "../Container/index_page/navbar";
-const IndexLayout = () => {
-    return (_jsxs("div", { children: [_jsx(Navbar, {}), _jsx(Layout, { style: { minHeight: "100vh" }, children: _jsx(Layout.Content, { style: { padding: "20px" }, children: _jsx(Outlet, {}) }) })] }));
-};
-export default IndexLayout;
