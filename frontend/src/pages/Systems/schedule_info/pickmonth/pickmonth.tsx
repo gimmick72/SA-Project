f3e51f0bcc7f598ed5b908bb07e96f2d370b08f6
JC-2547 <jetsada8648@gmail.com>
2025-09-04 17:08:14 +0700

    nay03

--- frontend/src/pages/Systems/schedule_info/pickmonth/pickmonth.tsx
+++ frontend/src/pages/Systems/schedule_info/pickmonth/pickmonth.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { DemoContainer } from '@c/internals/demo';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
