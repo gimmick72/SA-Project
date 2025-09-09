df2f9c7ef08f711a7664c3a7513b3923f0efd08c
ChakeawNO1 <158066468+ChakeawNO1@users.noreply.github.com>
2025-09-09 14:29:58 +0700

    UIใหม่

--- frontend/src/routes/HomeRoutes.tsx
+++ frontend/src/routes/HomeRoutes.tsx
@@ -2,12 +2,10 @@ import { lazy } from "react";
 import type { RouteObject } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Loadable from "../components/third-patry/Loadable";
+import BookingPage from "../pages/First_pages/First_pages/BookingPage/BookingPage";
 
 // HomePage
 const FirstPages = Loadable(lazy(() => import("../pages/First_pages/main")));
-// const BookingPage = Loadable(lazy(() => import("../pages/First_pages/First_pages/booking_page/index")));
-const AllBooking = Loadable(lazy(() => import("../pages/First_pages/First_pages/booking_page/index")));
-
 
 const IndexRoutes: RouteObject[] = [
   {
@@ -19,8 +17,8 @@ const IndexRoutes: RouteObject[] = [
         element: <Navigate to="/home" replace />, // redirect ไป /home
       },
       {
-        path: "Allbooking",
-        element: <AllBooking />,
+        path: "booking",
+        element: <BookingPage/>,
       },
 
     ],
