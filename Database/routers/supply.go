3ed53fc2f0655addfabfa10e86d5c830c02dab8e
JC-2547 <jetsada8648@gmail.com>
2025-09-10 16:09:12 +0700

    30 %

--- Database/routers/supply.go
+++ Database/routers/supply.go
@@ -7,7 +7,7 @@ import (
 
 
 func SupplyRouter(router *gin.RouterGroup) {
-			router.GET("/supplies", controllers.ListSupplies)
+		router.GET("/supplies", controllers.ListSupplies)
 		router.POST("/supplies", controllers.CreateSupply)
 		router.DELETE("/supplies/:id", controllers.DeleteSupply)
 		router.POST("/dispenses", controllers.CreateDispense)
